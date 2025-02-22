ff75fdf4d044fc0074de2b404701eec56584d5cf
fred olotoin <43315244+freduck@users.noreply.github.com>
2025-02-22 14:42:57 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,1 +1,19 @@
+<?php 
+
+//var_dump($_POST);
+extract($_POST);
+
+
+
+require "Conn.php";
+
+$login=mysqli_query($conn,"SELECT * FROM admin WHERE email='$username' AND password='$password'");
+  if(mysqli_num_rows($login)>0){
+    session_start();
+    $_SESSION['email']=$username;
+   
+  header("location:dashboard.php?url=tables");
+  }else{
+    header ("location: adminLogin.php");
+  }
 
