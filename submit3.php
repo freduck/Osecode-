c80e2a6b0bb59c2a0dc9af1eb9d6e2abd6a5fcf7
fred olotoin <43315244+freduck@users.noreply.github.com>
2025-02-23 15:25:30 +0100

    Add files via upload

---
+++ submit3.php
@@ -0,0 +1,22 @@
+<?php
+require "Conn.php";
+
+
+//var_dump($_POST);
+extract($_POST);
+$choice=mysqli_real_escape_string($conn,$choice);
+$check=mysqli_query($conn,"SELECT *FROM loop_answer WHERE qid='$id' AND email='$email'");
+if(mysqli_num_rows($check)>0){
+echo "<script> alert('This Question Has Been Answered'); window.location='profile.php?url=ifstaments';</script>";
+exit ();
+}
+$insert=mysqli_query($conn,"INSERT INTO loop_answer (qid, email,correct_answer)VALUES('$id','$email','$choice')");
+if($insert){
+
+header("location:profile.php?url=loops");
+}else{
+ echo mysqli_error($conn);
+
+}
+
+?>
